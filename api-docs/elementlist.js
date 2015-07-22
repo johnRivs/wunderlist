369c5791938ab895631bbeac4a3a573b3eba8964
John Rivs <wyathfallensoul@gmail.com>
2015-07-22 02:17:25 +0200

    Update API Docs

--- api-docs/elementlist.js
+++ api-docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","JohnRivs\\Wunderlist\\Authorization"],["c","JohnRivs\\Wunderlist\\Folder"],["c","JohnRivs\\Wunderlist\\Lists"],["c","JohnRivs\\Wunderlist\\Note"],["c","JohnRivs\\Wunderlist\\Reminder"],["c","JohnRivs\\Wunderlist\\Subtask"],["c","JohnRivs\\Wunderlist\\Task"],["c","JohnRivs\\Wunderlist\\User"],["c","JohnRivs\\Wunderlist\\Webhook"],["c","JohnRivs\\Wunderlist\\Wunderlist"]];
+ApiGen.elements = [["c","JohnRivs\\Wunderlist\\Authorization"],["c","JohnRivs\\Wunderlist\\Avatar"],["c","JohnRivs\\Wunderlist\\Comment"],["c","JohnRivs\\Wunderlist\\Folder"],["c","JohnRivs\\Wunderlist\\Lists"],["c","JohnRivs\\Wunderlist\\Note"],["c","JohnRivs\\Wunderlist\\Reminder"],["c","JohnRivs\\Wunderlist\\Subtask"],["c","JohnRivs\\Wunderlist\\Task"],["c","JohnRivs\\Wunderlist\\User"],["c","JohnRivs\\Wunderlist\\Webhook"],["c","JohnRivs\\Wunderlist\\Wunderlist"]];
